ab70c7b51b4fa4ff6708e2fb81e7de914cf47b8a
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-09-17 00:01:08 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,10 @@ var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 var request = require('request');
 var TikTokScraper = require('tiktok-scraper');
+const canvac = require('canvacord')
+const dcanvas = require('discord-canvas')
+
+
 var router  = express.Router();
 
 var { color, bgcolor } = require(__path + '/lib/color.js');
@@ -43,6 +47,19 @@ var {
     Gempa
 } = require('./../lib');
 var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
+
+const isUrl = (url) => {
+			return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)/, 'gi'))
+}
+
+const mess = {
+  noimg: 'Masukkan Parameter url gambar!',
+  error: {
+    status: 'error',
+    message: 'Sesuatu yang anda cari tidak ditemukan/error!'
+  },
+  url: 'Url Tidak Valid'
+}
 
 loghandler = {
     notparam: {
@@ -2688,4 +2705,26 @@ router.get('/igstalk', async(req, res) => {
         })
     });
 })
+
+//canvas
+router.get('/canvas/wasted', async(req, res) => {
+  const apikey = req.query.apikey
+  const img = req.query.img
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+        if(!apikey) return res.json(loghandler.notparam)
+    if(apikey != apii) return res.json(loghandler.invalidKey)
+  if(!img) return res.json(mess.noimg)
+  if(!isUrl(img)) return res.json(mess.url)
+  canvac.Canvas.wasted(img).then(data => {
+    canvac.write(data, './media/wasted.png')
+    res.sendFile('/app/media/wasted.png')
+  })
+  .catch(error => {
+    res.json(mess.error)
+  })
+})
+
 module.exports = router
